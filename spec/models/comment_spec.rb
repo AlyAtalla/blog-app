d4153174913b1739dbad2df0e05890e8969d6aa2
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-10-25 04:11:26 +0300

    update comment spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,24 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  describe 'Validations For the Comment model' do
+  describe 'Validations for the Comment model' do
     before(:each) do
       @comment = Comment.new(text: 'One comment', author_id: 11, post_id: 2)
     end
 
-    before { @comment.save }
+    it 'is valid with all attributes present' do
+      expect(@comment).to be_valid
+    end
 
-    it 'if title is present' do
+    it 'is not valid if text is not present' do
       @comment.text = nil
       expect(@comment).to_not be_valid
     end
 
-    it 'if author_id is integer' do
+    it 'is not valid if author_id is not an integer' do
       @comment.author_id = 'W'
       expect(@comment).to_not be_valid
     end
 
-    it 'if post_id is integer' do
+    it 'is not valid if post_id is not an integer' do
       @comment.post_id = 'Q'
       expect(@comment).to_not be_valid
     end
