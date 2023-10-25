61525bbae9117a1b9960b8ebe6b674ab5e7fa38a
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-10-25 14:58:59 +0300

    Fix linter errors

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,8 +7,6 @@ RSpec.describe User, type: :model do
     subject.posts_counter = 5 # Change the value to an integer
     expect(subject).to be_valid
   end
-
-
   it 'User Posts Counter attribute should be greater or equal to zero' do
     subject.posts_counter = -1 # Change the value to a negative integer
     expect(subject).to_not be_valid
