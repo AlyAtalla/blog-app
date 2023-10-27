d6e9248ccd92e600eac0eb9a9f3c499048df66c5
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-10-27 14:27:21 +0200

    refactor models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,12 @@
 class Comment < ApplicationRecord
+  belongs_to :author, class_name: 'User'
   belongs_to :post
-  belongs_to :user
-  validates :text, presence: true, length: { maximum: 100 }
+
+  after_save :update_post_comments_counter
+
+  private
+
+  def update_post_comments_counter
+    post.update(comments_counter: post.comments.count)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ApplicationRecord
-  has_many :posts
+  has_many :posts, foreign_key: :author_id
   has_many :comments
   has_many :likes
+
+  validates :name, presence: true
+  validates :photo, presence: true
+  validates :bio, presence: true
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  def recent_posts
+    posts.order(created_at: :desc).limit(3)
+  end
 end
