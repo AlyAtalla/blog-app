ae5ab63db6dfbff9e73bc031d9738089798d251d
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-10-30 21:26:08 +0200

    request user spec

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe "Users", type: :request do
+  describe "GET /users" do
+    it "works! (now write some real specs)" do
+      get users_index_path
+      expect(response).to have_http_status(200)
+    end
   end
 end
