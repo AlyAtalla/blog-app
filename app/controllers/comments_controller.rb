8d923b8587ca11efece5447ba87fce546ea5d10a
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-11-01 22:33:33 +0200

    Add create method to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,4 +8,20 @@ class CommentsController < ApplicationController
     @comment = Comment.find(params[:id])
     render status: :ok
   end
+
+  def create
+    @comment = Comment.new(comment_params)
+    
+    if @comment.save
+      render json: @comment, status: :created
+    else
+      render json: @comment.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
