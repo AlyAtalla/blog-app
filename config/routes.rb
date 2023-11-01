bcdc056b725e4daa25311fe75969e6c96a16fa26
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-11-01 22:34:38 +0200

    add new method to comment in routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "users#index"
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :create, :new] do
-      resources :comments, only: [:create]
+      resources :comments, only: [:index, :show, :create]
       post "like", on: :member
     end
   end
