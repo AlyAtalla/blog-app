8b6695b252a7f315f781fbbe5d3b7bf0eaa94884
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-11-06 11:55:25 +0200

    update models

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,7 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :posts_counter, numericality: { greater_than_or_equal_to: 0, allow_nil: true }
+
   after_save :update_user_posts_counter
 
   def recent_comments
