a7c399ae8c8d9c34e050d0fc4103dba7846a3d15
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-11-15 13:04:02 +0200

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-  has_many :posts, foreign_key: :author_id
+
+  has_many :posts, foreign_key: 'author_id'
   has_many :comments
   has_many :likes
 
   validates :name, presence: true
-  validates :photo, presence: true
-  validates :bio, presence: true
-  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0, allow_nil: true }
+  validates :posts_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  def recent_posts
-    posts.order(created_at: :desc).limit(3)
+  def three_recent_posts
+    posts.order(created_at: :desc).first(3)
   end
 end
