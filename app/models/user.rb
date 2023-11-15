d15e3cfc2e885d6a8ea35d381828ba6a34b424a3
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-11-15 14:30:56 +0200

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,24 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-
+ 
   has_many :posts, foreign_key: 'author_id'
   has_many :comments
   has_many :likes
-
+ 
   validates :name, presence: true
   validates :posts_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-
+ 
+  before_save :set_default_posts_counter
+ 
   def three_recent_posts
     posts.order(created_at: :desc).first(3)
   end
-end
+ 
+  private
+ 
+  def set_default_posts_counter
+    self.posts_counter ||= 0
+  end
+ end
+ 
