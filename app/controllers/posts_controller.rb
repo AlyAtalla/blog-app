aba4218202f03fbabbcd36d51203ab0a0b72d299
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-11-17 12:31:39 +0200

    update posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,15 +6,20 @@ class PostsController < ApplicationController
     @posts = Post.where(author_id: params[:user_id]).order(id: :asc)
     @posts = @posts.paginate(page: params[:page], per_page: 5)
   end
-
   def show
-    @post = Post.find(params[:id])
+    @user = User.find(params[:user_id])
+    @post = Post.includes(:author, :comments, :likes).find_by(author_id: @user.id, id: params[:id])
+    if @post
+      @comment = Comment.new
+      @like = Like.new
+    else
+      flash[:alert] = 'Post not found!'
+      redirect_to user_post_path(@user, params[:id])
+    end
   end
-
   def new
     @post = Post.new
   end
-
   def create
     @post = Post.new(post_params)
     @post.author = current_user
@@ -26,7 +31,6 @@ class PostsController < ApplicationController
       render :new, locals: { post: @post }
     end
   end
-
   def destroy
     @post = Post.find(params[:id])
     @post.author.decrement!(:posts_counter)
@@ -34,9 +38,7 @@ class PostsController < ApplicationController
     flash[:success] = 'Post was deleted successfully!'
     redirect_to user_posts_url
   end
-
   private
-
   def post_params
     params.require(:post).permit(:title, :text)
   end
