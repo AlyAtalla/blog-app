0edea65d4697e0d875590824509af285c836e14f
AlyAtalla <aliatalla93@gmail.com>
2025-08-16 20:42:59 +0300

    update work files

--- app/controllers/api/posts_controller.rb
+++
@@ -1,6 +0,0 @@
-class Api::PostsController < Api::ApplicationController
-  def index
-    @posts = Post.where(author_id: params[:user_id]).order(id: :asc)
-    render json: @posts, status: :ok
-  end
-end
